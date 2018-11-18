ace3beda75db8dcb297ab7c7802c718ec1110736
Mikel Loidi <mikel.loidi@outlook.com>
2018-11-18 13:39:44 +0100

    improve accessibility

--- src/Components/NavMenu.js
+++ src/Components/NavMenu.js
@@ -3,46 +3,8 @@ import { NavLink as Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 // My components
-import NavStyles from './Styles/NavStyles';
+import { NavStyles, StyledHeader, Logo} from './Styles/NavStyles';
 
-const Logo = styled.h1`
-  font-size: 0.5rem;
-  margin-left: 2rem;
-  position: relative;
-  z-index: 2;
-  transform: skew(-7deg);
-  a {
-    padding: 0.5rem 1rem;
-    background: green;
-    color: white;
-    text-transform: uppercase;
-    text-decoration: none;
-  }
-  @media (max-width: 1300px) {
-    margin: 0;
-    text-align: center;
-  }
-`;
-
-const StyledHeader = styled.header`
-  .bar {
-    border-bottom: 10px solid black;
-    display: grid;
-    grid-template-columns: auto 1fr;
-    justify-content: space-between;
-    align-items: stretch;
-    @media (max-width: 1300px) {
-      grid-template-columns: 1fr;
-      justify-content: center;
-    }
-  }
-
-  .sub-bar {
-    display: grid;
-    grid-auto-columns: 1fr auto;
-    border-bottom: 1px solid #e1e1e1;
-  }
-`;
 
 class NavMenu extends Component {
   render() {
@@ -58,10 +20,18 @@ class NavMenu extends Component {
             </Link>
           </Logo>
           <NavStyles>
-            <Link to={'/'}>About me</Link>
-            <Link to={'/resume'}>Work Experience</Link>
-            <Link to={'/education'}>Education</Link>
-            <Link to={'/projects'}>My projects</Link>
+            <li>
+              <Link to={'/'}>About me</Link>
+            </li>
+            <li>
+              <Link to={'/resume'}>Work Experience</Link>
+            </li>
+            <li>
+              <Link to={'/education'}>Education</Link>
+            </li>
+            <li>
+              <Link to={'/coursesprojects'}>Courses & projects</Link>
+            </li>
           </NavStyles>
         </div>
         <div className="sub-bar" />
