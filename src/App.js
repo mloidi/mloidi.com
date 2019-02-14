fbe1774c9905985025fe19f104cfed0619465b22
Mikel Loidi Ardanaz <mloidi@MacBook-Pro-de-Mikel.local>
2019-02-14 21:30:36 +0100

    Still working on it

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import Menu from './Components/Menu';
-import About from './Components/About';
-import Work from './Components/Work';
-import Education from './Components/Education';
-import Skills from './Components/Skills';
-import Courses from './Components/Courses';
-import Projects from './Components/Projects';
-import NotFound from './Components/NotFound';
+// import Menu from './Components/Menu';
+// import About from './Components/About';
+// import Work from './Components/Work';
+// import Education from './Components/Education';
+// import Skills from './Components/Skills';
+// import Courses from './Components/Courses';
+// import Projects from './Components/Projects';
+// import NotFound from './Components/NotFound';
 import Construction from './Components/Construction';
 
 const MainContainer = styled.div`
