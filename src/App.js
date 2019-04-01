e40b7743a1644b8db44b4c6d254e9ccc3490dd9b
Mikel Loidi Ardanaz <mloidi@MacBook-Pro-de-Mikel.local>
2019-04-01 18:46:52 +0200

    remove dotenv

--- src/App.js
+++ src/App.js
@@ -13,8 +13,6 @@ import NotFound from './Components/Pages/NotFound';
 
 import { MainContainer } from './Components/Style/Style';
 
-require('dotenv').config();
-
 class App extends Component {
   render() {
     return (
