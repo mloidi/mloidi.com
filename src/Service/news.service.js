a848d2111b6d6cd60adf9ce99d2ebcdda0a89415
Mikel Loidi Ardanaz <mloidi@MacBook-Pro-de-Mikel.local>
2019-04-01 19:11:18 +0200

    update react

--- src/Service/news.service.js
+++ src/Service/news.service.js
@@ -1,7 +1,7 @@
 const url =
   process.env.REACT_APP_NEWS_URL +
   'sources?language=en&category=technology' +
-//   'everything?q=react&language=en&from=2019-03-31&to=2019-03-31&sortBy=popularity' +
+  //   'everything?q=react&language=en&from=2019-03-31&to=2019-03-31&sortBy=popularity' +
   '&apiKey=' +
   process.env.REACT_APP_NEWS_API;
 
