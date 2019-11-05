1d9666bf2a493e6fab89259cf13faa5bd8aa0fe6
Mikel Loidi Ardanaz <mloidi@MacBook-Pro-de-Mikel.local>
2019-11-05 22:00:53 +0100

    new legal page

--- src/components/Cookies.jsx
+++ src/components/Cookies.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 import { SectionTitle, SectionContent, PageDiv } from './style/Style';
 const Cookies = () => {
   return (
-    <PageDiv id="courses">
-      <SectionTitle>Cookie Policy</SectionTitle>
-      <SectionContent>
-        <p>
+    <div className="mt-8 mx-4 lg:mx-24 max-h-full text-gray-500">
+      <div className="text-2xl mb-4 text-gray-900">Cookie Policy</div>
+      <div className="my-2">
+        <div className="my-1">
           This Cookie Policy explains how cookies and similar technologies
           (collectively, “Cookie(s)”) are used when you visit our Site. A “Site”
           includes our websites, emails, and other applications owned and
@@ -14,20 +14,20 @@ const Cookies = () => {
           other services that display this Cookie Policy. This policy explains
           what these technologies are and why they are used, as well as your
           right to control their use.
-        </p>
-        <p>
+        </div>
+        <div className="my-1">
           If you have any question, please contact us by email at
           privacy@mloidi.com.
-        </p>
-        <h2>1. What Is A Cookie?</h2>
-        <p>
+        </div>
+        <div className="text-xl my-2 text-gray-700">1. What Is A Cookie?</div>
+        <div className="my-1">
           A cookie is a small text file (often including a unique identifier),
           that is sent to a user’s browser from a website's computers and stored
           on a user’s computer's hard drive or on a tablet or mobile device
           (collectively, “Computer”). A Cookie stores a small amount of data on
           your Computer about your visit to the Site.
-        </p>
-        <p>
+        </div>
+        <div className="my-1">
           We may also use “web beacons” (also known as “clear GIFs” or “pixel
           tags”) or similar technologies on our Site to enable us to know
           whether you have visited a web page or received a message. A web
@@ -43,8 +43,8 @@ const Cookies = () => {
           information about you, including your personal information. In many
           instances, web beacons rely on cookies to function properly, and so
           declining cookies will impair their functioning.
-        </p>
-        <p>
+        </div>
+        <div className="my-1">
           We place and access Cookies on your Computer; these Cookies are known
           as “first-party Cookies.” Cookies may also be placed and accessed by
           some of our third-party vendors, which are known as “third-party
@@ -53,28 +53,30 @@ const Cookies = () => {
           your device while you are visiting our Site. Other Cookies are
           "persistent Cookies," which means that they are stored on your device
           for a period of time after you leave our Site.
-        </p>
-        <p>
+        </div>
+        <div className="my-1">
           You can choose whether to accept Cookies by changing the settings on
           your browser. However, if you disable this function (or keep this
           function disabled, as set by default by certain browsers), your
           experience on the Site may be diminished and some features may not
           work as intended.
-        </p>
-        <h2>2. What Cookies Do We Use?</h2>
-        <p>
+        </div>
+        <div className="text-xl my-2 text-gray-700">
+          2. What Cookies Do We Use?
+        </div>
+        <div className="my-1">
           Below we list the different types of Cookies that are used on the Site
           that you are visiting.
-        </p>
-        <p>
+        </div>
+        <div className="my-1">
           To the extent any personal information is collected through
           first-party Cookies, our Privacy Policy applies and complements this
           Cookie Policy. Personal information collected through a third-party
           Cookie is subject to the privacy policy of that third party, and not
           our Privacy Policy.
-        </p>
-        <h3>Analytics Cookies</h3>
-        <p>
+        </div>
+        <div className="my-1 text-gray-700">Analytics Cookies</div>
+        <div className="my-1">
           Analytics Cookies collect information about your use of the Site, and
           enable us to improve the way it works. For example, Analytics Cookies
           show us which are the most frequently visited pages on the Site, help
@@ -84,26 +86,29 @@ const Cookies = () => {
           single person. We use information from Analytics Cookies to analyze
           the Site traffic, but we do not examine this information for
           individually identifying information.
-        </p>
-        <p>
+        </div>
+        <div className="my-1">
           Cookie: _ga, _gtag_UA_129704170_1, _gid, <br />
           Description: Google Analytics <br />
           Duration: 1 year, 6 months <br />
           Privacy policy:{' '}
           <a
+            className="border-b rounded border-gray-700"
             href="https://policies.google.com/privacy"
             target="_blank"
             rel="noopener noreferrer"
           >
             go to site
           </a>
-        </p>
-        <h2>3. How Do I Manage Cookies?</h2>
-        <p>
+        </div>
+        <div className="text-xl my-2 text-gray-700">
+          3. How Do I Manage Cookies?
+        </div>
+        <div className="my-1">
           You can change your Cookie settings above by opting out of certain or
           all Cookies.
-        </p>
-        <p>
+        </div>
+        <div className="my-1">
           In addition to the options provided above, you may refuse or accept
           Cookies from the Site or any other website at any time by activating
           settings on your browser. Most browsers automatically accept Cookies,
@@ -112,56 +117,62 @@ const Cookies = () => {
           sign in or use other interactive features of our Site that depend on
           Cookies. Information about the procedure to follow in order to enable
           or disable Cookies can be found at:
-        </p>
-        <p>
+        </div>
+        <div className="my-1">
           <a
+            className="border-b rounded border-gray-700"
             href="https://support.google.com/chrome/answer/95647?hl=en"
             target="_blank"
             rel="noopener noreferrer"
           >
             Chrome
           </a>
-        </p>
-        <p>
+        </div>
+        <div className="my-1">
           <a
+            className="border-b rounded border-gray-700"
             href="https://support.apple.com/guide/safari/manage-cookies-and-website-data-sfri11471/mac"
             target="_blank"
             rel="noopener noreferrer"
           >
             Safari
           </a>
-        </p>
-        <p>
+        </div>
+        <div className="my-1">
           <a
+            className="border-b rounded border-gray-700"
             href="https://support.apple.com/en-us/HT201265"
             target="_blank"
             rel="noopener noreferrer"
           >
             Safari Mobile (iPhone and iPads)
           </a>
-        </p>
-        <p>
+        </div>
+        <div className="my-1">
           <a
+            className="border-b rounded border-gray-700"
             href="https://support.mozilla.org/en-US/kb/clear-cookies-and-site-data-firefox?redirectlocale=en-US&redirectslug=delete-cookies-remove-info-websites-stored"
             target="_blank"
             rel="noopener noreferrer"
           >
             Firefox
           </a>
-        </p>
-        <p>
+        </div>
+        <div className="my-1">
           <a
+            className="border-b rounded border-gray-700"
             href="https://support.microsoft.com/en-us/help/17442/windows-internet-explorer-delete-manage-cookies"
             target="_blank"
             rel="noopener noreferrer"
           >
             Microsoft Edge
           </a>
-        </p>
-        <p>
+        </div>
+        <div className="my-1">
           For more information about other commonly used browsers, please refer
           to{' '}
           <a
+            className="border-b rounded border-gray-700"
             href="http://www.allaboutcookies.org/manage-cookies/"
             target="_blank"
             rel="noopener noreferrer"
@@ -169,16 +180,17 @@ const Cookies = () => {
             http://www.allaboutcookies.org/manage-cookies/
           </a>
           .
-        </p>
-        <p>Please be aware that</p>
-        <p>
+        </div>
+        <div className="my-1">Please be aware that</div>
+        <div className="my-1">
           if Cookies are disabled, not all features of the Site may operate as
           intended.
-        </p>
-        <p>
+        </div>
+        <div className="my-1">
           To opt-out of participating in Google Analytics data follow the
           instructions:{' '}
           <a
+            className="border-b rounded border-gray-700"
             href="https://tools.google.com/dlpage/gaoptout"
             target="_blank"
             rel="noopener noreferrer"
@@ -186,8 +198,8 @@ const Cookies = () => {
             https://tools.google.com/dlpage/gaoptout
           </a>
           .
-        </p>
-        <p>
+        </div>
+        <div className="my-1">
           If the Site has ads, some of the network advertisers, ad agencies and
           other vendors we work with to serve ads on our Site, third-party
           websites and across the internet, or analytics providers that provide
@@ -198,6 +210,7 @@ const Cookies = () => {
           Alliance. To opt out from receiving interest-based advertising from
           some or all of the participating companies, please visit{' '}
           <a
+            className="border-b rounded border-gray-700"
             href="http://www.aboutads.info/choices/"
             target="_blank"
             rel="noopener noreferrer"
@@ -206,6 +219,7 @@ const Cookies = () => {
           </a>{' '}
           and
           <a
+            className="border-b rounded border-gray-700"
             href="http://www.youronlinechoices.eu/"
             target="_blank"
             rel="noopener noreferrer"
@@ -217,9 +231,9 @@ const Cookies = () => {
           types of online advertising from participating companies and any type
           of advertising from non-participating companies, and the web sites you
           visit may still collect information for other purposes.
-        </p>
-      </SectionContent>
-    </PageDiv>
+        </div>
+      </div>
+    </div>
   );
 };
 
